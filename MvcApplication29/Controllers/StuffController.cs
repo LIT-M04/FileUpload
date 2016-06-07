39189fdc0affb4b43da0d34eae1cb61ac62f19f7
Avrumi Friedman <info@lakewoodprogramming.com>
2016-06-07 12:42:28 -0400

    Initial commit

---
+++ MvcApplication29/Controllers/StuffController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Stuff.DAta;
+
+namespace MvcApplication29.Controllers
+{
+    public class StuffController : Controller
+    {
+        public ActionResult Index()
+        {
+            var manager = new StuffManager(Properties.Settings.Default.ConStr);
+            return View(manager.GetStuff());
+        }
+
+        [HttpPost]
+        public ActionResult Submit(string word, HttpPostedFileBase image)
+        {
+            string fileName = Guid.NewGuid() + Path.GetExtension(image.FileName);
+            image.SaveAs(Server.MapPath("~/Images/") + fileName);
+            var manager = new StuffManager(Properties.Settings.Default.ConStr);
+            manager.AddStuff(fileName, word);
+            return Redirect("/stuff/index");
+        }
+
+    }
+}
---
+++ Stuff.DAta/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stuff.DAta
+{
+    public class Stuff
+    {
+        public int Id { get; set; }
+        public string Word { get; set; }
+        public string Image { get; set; }
+    }
+
+    public class StuffManager
+    {
+        private string _connectionString;
+
+        public StuffManager(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IEnumerable<Stuff> GetStuff()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var cmd = connection.CreateCommand();
+                cmd.CommandText = "SELECT * FROM Stuffs";
+                List<Stuff> stuffs = new List<Stuff>();
+                connection.Open();
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    stuffs.Add(new Stuff
+                    {
+                        Id = (int)reader["Id"],
+                        Image = (string)reader["Image"],
+                        Word = (string)reader["Word"]
+                    });
+                }
+
+                return stuffs;
+            }
+        }
+
+        public void AddStuff(string image, string word)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var cmd = connection.CreateCommand();
+                cmd.CommandText = "INSERT INTO Stuffs (Word, Image) VALUES (@word, @image)";
+                cmd.Parameters.AddWithValue("@word", word);
+                cmd.Parameters.AddWithValue("@image", image);
+                connection.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
